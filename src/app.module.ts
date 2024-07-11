5877312146530315ee3de45c78e5dc25dcc4f25b
MitkeMike <memb8631@gmail.com>
2024-07-11 15:25:22 -0600

    Create the enviroments variables and do the connection to the postgresql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true, //Change the entities
+      synchronize: true, // Don't use this in production
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..','public'),
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
